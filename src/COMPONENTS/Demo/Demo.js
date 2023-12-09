70051fba6a4971f0e64ba90b4f45eae9abe1c323
Keshav Singh <keshavy561@gmail.com>
2023-12-09 16:41:58 +0530

    redux state added]

--- src/COMPONENTS/Demo/Demo.js
+++ src/COMPONENTS/Demo/Demo.js
@@ -1,27 +1,43 @@
 import React from 'react';
-
-import dataUsers from '../../data/users.js';
+import { useDispatch, useSelector } from 'react-redux';
 
 function Demo() {
-  const [users, setUsers] = React.useState(dataUsers);
+  const data = useSelector((store) => store.users);
+  const dispatch = useDispatch();
+
+  function handleAdd() {
+    dispatch({
+      type: 'users/add',
+      payload: {
+        name: 'Keshav',
+        email: 'keshav@gmail.com',
+      },
+    });
+  }
 
   return (
-    <div className='d-flex' style={{ gap: '20px', flexWrap: 'wrap' }}>
-      {users.map((user, i) => (
-        <div class='card' style={{ width: '18rem', margin: '20px' }}>
-          <div class='card-body'>
-            <h5 class='card-title'>{user.name}</h5>
-            <p class='card-text'>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href='#' class='btn btn-primary'>
-              {user.email}
-            </a>
+    <>
+      <div className='d-flex' style={{ gap: '20px', flexWrap: 'wrap' }}>
+        {data.map((user, i) => (
+          <div className='card' style={{ width: '18rem', margin: '20px' }}>
+            <div className='card-body'>
+              <h5 className='card-title'>{user.name}</h5>
+              <p className='card-text'>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href='#' class='btn btn-primary'>
+                {user.email}
+              </a>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+      <button className='primary primary-btn' onClick={handleAdd}>
+        {' '}
+        Add{' '}
+      </button>
+    </>
   );
 }
 
